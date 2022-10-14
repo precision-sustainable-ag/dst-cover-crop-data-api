c93e262b612a82cdfca2db0398c5a513c566d8ee
trevorjpuckett <33693273+trevorjpuckett@users.noreply.github.com>
2022-10-13 20:40:35 -0400

    python image seeder

--- src/migrations/fresh.js
+++ src/migrations/fresh.js
@@ -1,5 +1,25 @@
 const {ModelsProvider} = require('../app/providers/ModelsProvider');
 const {DatabaseProvider} = require('../app/providers/DatabaseProvider');
+const {Client} = require('pg');
 
+const settings = DatabaseProvider.settings();
 
-DatabaseProvider.sync(ModelsProvider,{force:true});
+const pgClient = new Client({
+    user: settings.username,
+    password: settings.password,
+    host: settings.host,
+    database: 'postgres'
+});
+
+const CreateDatabaseIfNotExists = "SELECT 'CREATE DATABASE cover_crops' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'cover_crops')"
+
+pgClient.connect();
+
+pgClient.query(CreateDatabaseIfNotExists, (err, res) => {
+    if(res){
+        console.log('response',res);
+        DatabaseProvider.sync(ModelsProvider,{force:true});
+    }
+    pgClient.end();
+})
+
