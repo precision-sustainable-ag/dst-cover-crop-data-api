c2c80a0d2e4e8fae7ef87200f5825f2f683dd807
trevorjpuckett <33693273+trevorjpuckett@users.noreply.github.com>
2022-10-16 12:45:37 -0400

    paginated request max limit

--- src/config/pagination.js
+++ src/config/pagination.js
@@ -3,7 +3,7 @@ const {env} = require('./kernel');
 module.exports = {
     default: {
         page: 1,
-        limit: env.PAGINATION_DEFAULT_LIMIT ?? 10,
+        limit: env.PAGINATION_DEFAULT_LIMIT ?? 0,
         maxLimit: env.PAGINATION_DEFAULT_MAX_LIMIT ?? 500,
     }
 }
