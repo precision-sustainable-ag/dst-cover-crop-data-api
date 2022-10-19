25920b4d803bd8264a12125721c76aa12b4c6541
trevorjpuckett <33693273+trevorjpuckett@users.noreply.github.com>
2022-10-18 21:07:33 -0400

    PostgresService class

---
+++ _sandbox/db-listen.js
@@ -0,0 +1,80 @@
+const {Client} = require('pg');
+const { PostgresService } = require('../src/app/services/database/PostgresService');
+const settings = require('../src/config/database');
+
+
+const database = 'cover_crops';
+
+
+function getClient(){
+    return new Client({
+        user: settings.username,
+        password: settings.password,
+        host: settings.host,
+        database: database
+    });
+}
+
+function basic(){
+    const client = getClient();
+    client.connect();
+    // do stuff
+    console.log('>>> CONNECTED',settings);
+    
+    client.query('LISTEN test');
+    
+    client.on('notification',(msg) => {
+        console.log('>> MSG',msg);
+    });
+    
+    client.query(`NOTIFY test, '!THIS IT A TEST!'`)
+}
+
+function iteration2(){
+    const client = getClient();
+    const client2 = getClient();
+
+    client.connect().then(()=>{
+    
+        client.query('LISTEN test').then(()=>{
+            client.on('notification', (msg)=>{
+                console.log('>> MSG', msg);
+            })
+            console.log('>>> Listening ...');
+        });
+    
+    })
+    
+    
+    client2.connect().then(()=>{
+        client2.query(`NOTIFY test, 'POOP'`).then(()=>{
+            console.log('notified.');
+            client2.end();
+        })
+    })
+}
+
+
+
+async function iteration3(){
+    const service = new PostgresService(settings);
+
+    await service.open({database:'postgres'})
+        .createDatabaseIfNotExists('sample_create_db')
+        .execute().then(res => {
+            console.log('RES',res);
+            console.log('Created DB!');
+        });
+
+    await service.listen({channel:'test', callback:(payload)=>console.log('>> PAYLOAD',payload)});
+
+    service.query({
+        sql: `NOTIFY test, '!THIS IS A TEST!'`,
+    }).execute();
+
+
+}
+
+iteration3();
+
+
---
+++ src/app/services/database/PostgresService.js
@@ -0,0 +1,156 @@
+const { Client } = require("pg");
+
+
+
+class PostgresService {
+
+
+    constructor(settings){
+
+        this.settings = settings;
+        this.queries = [];
+        this.watching = {};
+        this.client = null;
+        this.listener = null;
+
+    }
+
+    buildClient(database, settings){
+        if(!settings) settings = this.settings;
+        if(!database) database = settings.database;
+
+        return new Client({
+            user: settings.username,
+            password: settings.password,
+            host: settings.host,
+            database: database
+        });
+    }
+
+    async buildListener(database, settings){
+        if(this.listener) return;
+
+        this.listener = this.buildClient(database, settings);
+        await this.listener.connect();
+
+        const watching = this.watching;
+
+        this.listener.on('notification', (msg) => {
+            const channel = msg.channel;
+            if(channel in watching){
+                const callback = watching[channel];
+                callback(msg.payload);
+            }
+        })
+    }
+
+    async listen({channel, callback, database, settings}){
+        if(!this.listener) this.buildListener(database, settings);
+
+        await this.listener.query(`LISTEN ${channel}`);
+        this.watching[channel] = callback;
+    }
+
+    open({client, database, settings}){
+        if(this.client) return this.client
+
+        if(!client) client = this.buildClient(database,settings);
+        this.client = client;
+        
+        return this;
+    }
+
+    async execute(config={}){
+        if(!this.client) this.open(config);
+
+        const results = [];
+        await this.client.connect();
+
+        for(let query of this.queries){
+            const result = await this.client.query(query.sql,query.params);
+            const output = await query.resolver(result);
+            results.push(output);
+            if(output === false) break;
+        }
+
+        await this.client.end();
+        this.client = null;
+        this.queries = [];
+
+        return results;
+    }
+
+    query({sql, params, resolver}){
+        if(!params) params = [];
+        if(!resolver) resolver = () => true;
+
+        this.queries.push({
+            sql, params, resolver
+        });
+
+        return this;
+    }
+
+    findExistingDatabase(database){
+
+        const sql = `
+            SELECT true 
+            WHERE EXISTS (
+                SELECT FROM pg_database WHERE datname = $1::text
+            )`;
+        
+
+        return this.query({
+            sql,
+            params: [database],
+            resolver: (result) => result.rowCount >=1
+        });
+    }
+    
+    findNonExistingDatabase(database){
+
+        const sql = `
+            SELECT true 
+            WHERE NOT EXISTS (
+                SELECT FROM pg_database WHERE datname = $1::text
+            )`;
+        
+
+        return this.query({
+            sql,
+            params: [database],
+            resolver: (result) => result.rowCount >=1
+        });
+    }
+
+    createDatabase(database){
+
+        const sql = `
+            CREATE DATABASE ${database}
+        `;
+
+        return this.query({
+            sql,
+            params: [],
+            resolver: (result) => true
+        });
+    }
+
+
+    createDatabaseIfNotExists(database){
+
+        return this
+            .findNonExistingDatabase(database)
+            .createDatabase(database);
+
+    }
+
+
+
+
+}
+
+
+module.exports = {
+    PostgresService
+}
