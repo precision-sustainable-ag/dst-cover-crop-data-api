4a95ef5d9b2a35417c4069df8484a6d56fda0480
trevorjpuckett <33693273+trevorjpuckett@users.noreply.github.com>
2022-10-25 08:34:31 -0400

    patch sql trigger

--- _sql/triggers/add-trigger-to-table.sql
+++ _sql/triggers/add-trigger-to-table.sql
@@ -1,3 +1,3 @@
 CREATE TRIGGER notify_data_edit
 AFTER INSERT OR UPDATE OR DELETE ON <table_name>
-    FOR EACH ROW EXECUTE FUNCTION notify_data_edit('<channel-name>');
+    FOR EACH ROW EXECUTE FUNCTION notify_data_edit('<model-name>');
